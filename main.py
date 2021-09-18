ef6bf8606381c9a9f2b20a66ead230ca393d9020
Nehal Sekhon <nehalsekhon@gmail.com>
2021-09-18 00:53:30 -0600

    Add login and create user page

--- main.py
+++ main.py
@@ -20,14 +20,23 @@ def hello_world():
 
 @app.route("/home/", methods=['GET', 'POST'])
 def home():
+    # use buttons to redirect users to login or create user page
     if request.method == 'POST':
         if request.form['Log In'] == 'login':
-            pass
+            pass # todo: if login button clicked take user to login page
         elif request.form['Create Account'] == 'createuser':
-            pass
+            pass # todo: if create account button pressed take user to register page
     elif request.method == 'GET':
         pass
     return render_template('home.html')
+
+@app.route("/login/")
+def login():
+    return render_template('login.html')
+
+@app.route("/register/")
+def register():
+    return render_template('register.html')
 
 
 if __name__ == "__main__":
