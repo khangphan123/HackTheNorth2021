235800cb6e482103db6f2327104a06ae2e771434
Nehal Sekhon <nehalsekhon@gmail.com>
2021-09-18 00:43:31 -0600

    Update Home Page

--- main.py
+++ main.py
@@ -15,9 +15,19 @@ app = Flask(__name__)
 
 
 @app.route("/")
-
 def hello_world():
     return "Helloo, I hope that we can submit this project by the end of the hackathon"
+
+@app.route("/home/", methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        if request.form['Log In'] == 'login':
+            pass
+        elif request.form['Create Account'] == 'createuser':
+            pass
+    elif request.method == 'GET':
+        pass
+    return render_template('home.html')
 
 
 if __name__ == "__main__":
